a99c3e52659ca4c9631489fc340e7d772829603f
AhmedShokry7 <ahmedshokry7@example.com>
2025-08-16 21:00:53 +0300

    creating edit note view

--- lib/ui/widgets/notes_item.dart
+++ lib/ui/widgets/notes_item.dart
@@ -1,61 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:notes/ui/views/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 12.0),
-      padding: const EdgeInsets.only(top: 18, bottom: 32, left: 12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color.fromARGB(255, 116, 197, 119),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              child: Text(
-                "Build your career with Ahmed Shokry",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const EditNoteView()),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 12.0),
+        padding: const EdgeInsets.only(top: 18, bottom: 32, left: 12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: const Color.fromARGB(255, 116, 197, 119),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
                 style: TextStyle(
-                  color: Colors.black.withValues(alpha: .6),
-                  fontSize: 20,
+                  color: Colors.black,
+                  fontSize: 28,
                   fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete_rounded,
-                size: 35,
-                color: Colors.black,
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Text(
+                  "Build your career with Ahmed Shokry",
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: .6),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete_rounded,
+                  size: 35,
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 32.0),
-            child: Text(
-              "May 21, 2025",
-              style: TextStyle(
-                color: Colors.black.withValues(alpha: .6),
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.only(right: 32.0),
+              child: Text(
+                "May 21, 2025",
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: .6),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
