39628afd2edd4170d7e0b54a32e90c0fcc5c80df
AhmedShokry7 <ahmedshokry7@example.com>
2025-08-17 18:13:52 +0300

    create fech note method

--- lib/cubits/notes_cubit/cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/cubit/notes_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes/models/note_model.dart';
 
@@ -6,4 +7,14 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() {
+    try {
+      var noteBox = Hive.box<NoteModel>("notes_box");
+      List<NoteModel> notes = noteBox.values.toList();
+      emit(NotesSucess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/ui/views/notes_view.dart
+++ lib/ui/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/notes_cubit/cubit/notes_cubit.dart';
 import 'package:notes/ui/widgets/custom_floating_action_btn.dart';
 import 'package:notes/ui/widgets/notes_view_body.dart';
 
@@ -7,9 +9,12 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      floatingActionButton: CustomFloatingActionBtn(),
-      body: NotesViewBody(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: const Scaffold(
+        floatingActionButton: CustomFloatingActionBtn(),
+        body: NotesViewBody(),
+      ),
     );
   }
 }
